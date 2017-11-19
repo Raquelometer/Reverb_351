ca92c86b2de882e3f344a0de9d7198c3322a6845
John W Nonnenmacher <jnonno@caen-vnc-vm01.engin.umich.edu>
2017-11-19 16:18:35 -0500

    added convolution with sine sweep

---
+++ convolveWithAudio.m
@@ -0,0 +1,13 @@
+%% Read in test audio
+
+[testAudio, ~] = audioread(testAudioName);
+
+%% Record maximum volume 
+volume = max(testAudio);
+
+%% Convolve test audio with the IR
+
+audioOut = conv(testAudio(:, 1), IR);
+
+%% Set maximum volume to what it was before convolution
+audioOut = audioOut ./ max(audioOut) .* volume;
