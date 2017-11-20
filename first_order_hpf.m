0749a770b1f25f264ec6c5d3527f370e11c0b110
Raquelometer <daphnaraz@Daphnas-MacBook-Pro.local>
2017-11-19 20:33:51 -0500

    added hpf and lpf, edited plot_sound.

---
+++ first_order_hpf.m
@@ -0,0 +1,50 @@
+[sound, Fs] = audioread('kick.wav');
+
+filtered_sound = sound;
+
+z1_right = 0;
+z1_left = 0;
+
+fc = 250;
+
+wc = 2.*pi.*fc./Fs;
+
+alpha = 2 - cos(wc);
+
+b1 = -alpha + sqrt(alpha.^2 - 1);
+
+a0 = 1 + b1;
+
+%db = 10.^(a1/20);
+
+
+for i = 1:size(sound)
+    
+    xn = sound(i, 1);
+    
+    yn_1 = z1_left;
+    
+    yn = a0.*xn - b1.*yn_1;
+    
+    z1_left = yn;
+    
+    filtered_sound(i, 1) = yn;
+    
+end
+
+
+for i = 1:size(sound)
+    
+    xn = sound(i, 2);
+    
+    yn_1 = z1_right;
+    
+    yn = a0.*xn - b1.*yn_1;
+    
+    z1_right = yn;
+    
+    filtered_sound(i, 2) = yn;
+    
+end
+
+audiowrite('new_kick.wav', filtered_sound, Fs);
--- plot_sound.m
+++ plot_sound.m
@@ -1,11 +1,14 @@
 % Function for plotting audio signal vs time
-function[] = plot_sound(y, fs, fig_num)
+function[] = plot_sound(y, fs, fig_num, title)
 
 dt = 1/fs;
 t = linspace(0, length(y)/fs, length(y));
 
 figure(fig_num)
 plot(t,y')
+title(title)
+xlabel('time (s)')
+ylable('amplitude')
 
 
 end
