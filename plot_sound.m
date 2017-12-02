4bb68218cb0448c098ec137a1032d377c1c4e473
Raquelometer <daphnaraz@Daphnas-MacBook-Pro.local>
2017-12-02 16:03:02 -0500

    edited plotsound

--- plot_sound.m
+++ plot_sound.m
@@ -1,17 +1,15 @@
 % Function for plotting audio signal vs time
-function[] = plot_sound(y, fs, fig_num, title)
+function[] = plot_sound(y, fs, fig_num)
 
 dt = 1/fs;
 t = linspace(0, length(y)/fs, length(y));
 
 figure(fig_num)
 plot(t,y')
-title(title)
-xlabel('time (s)')
-ylable('amplitude')
-
+%title(title)
 xlabel('time (s)')
 ylabel('amplitude')
+
 
 
 end
