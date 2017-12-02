6b036f981aff8a1e0e9ddb27227255a34f2e5606
Raquelometer <daphnaraz@Daphnas-MacBook-Pro.local>
2017-12-02 16:21:03 -0500

    added realtime FFT

--- plot_sound.m
+++ plot_sound.m
@@ -1,15 +1,21 @@
 % Function for plotting audio signal vs time
-function[] = plot_sound(y, fs, fig_num)
+function[] = plot_sound(y, fs, fig_num, title)
 
 dt = 1/fs;
 t = linspace(0, length(y)/fs, length(y));
 
 figure(fig_num)
 plot(t,y')
+<<<<<<< HEAD
 %title(title)
 xlabel('time (s)')
 ylabel('amplitude')
 
+=======
+title(title)
+xlabel('time (s)')
+ylable('amplitude')
+>>>>>>> daphna_branch
 
 
 end
